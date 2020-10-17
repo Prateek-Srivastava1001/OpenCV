42d80144abffae91f64ad4fb318e58db932ec7a7
Prateek Srivastava <70479905+PrateekNITH@users.noreply.github.com>
2020-10-17 11:47:37 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,9 +1,11 @@
 
 import pyautogui, time
-time.sleep(5)
+time.sleep(10)	#  10 Seconds to open target location
 f = open("junk.txt", 'r')
 
 for word in f:
 	pyautogui.typewrite(word)
 	pyautogui.press("enter")
 	
+
+f.close()
